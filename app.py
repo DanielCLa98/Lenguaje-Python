25b01e8f9969412cd1964c222471d3635c30b92a
DanielCLa98 <daniel-cla@hotmail.com>
2024-11-21 18:12:03 -0500

    Firts Commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
@@ -15,4 +16,5 @@ def check_number():
     return render_template('index.html', resultado=resultado)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
